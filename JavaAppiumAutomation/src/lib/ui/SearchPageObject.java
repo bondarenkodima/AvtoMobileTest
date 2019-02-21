f31e43836fb6a5b801cb2047b424bb3900cacf9b
Dima <bondarenkodimay@gmail.com>
2019-02-21 21:10:27 +0300

    Ex9_Refactoring_Templates

--- JavaAppiumAutomation/src/lib/ui/SearchPageObject.java
+++ JavaAppiumAutomation/src/lib/ui/SearchPageObject.java
@@ -2,6 +2,7 @@ package lib.ui;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class SearchPageObject extends MainPageObject {
 
@@ -12,7 +13,8 @@ public class SearchPageObject extends MainPageObject {
             SEARCH_RESULT_BY_SUBSTRING_TPL = "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",   //ожидает элемент по ресурс ид и тексту из /* TEMPLATES METHODS */
             SEARCH_RESULT_ELEMENT = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
             SEARCH_EMPTY_RESULT_ELEMENT = "//*[@text='No results found']",
-            SEARCH_EMPTY_SEARCH = "//*[contains(@text, 'Search and read the free encyclopedia in your language')]"; // поиск лейба Search and read the free encyclopedia in your language на странице
+            SEARCH_EMPTY_SEARCH = "//*[contains(@text, 'Search and read the free encyclopedia in your language')]", // поиск лейба Search and read the free encyclopedia in your language на странице
+            SEARCH_BY_TITLE_AND_DESCRIPTION_TPL = "//*[@text='{TITLE}']/following-sibling::*[@text='{DESCRIPTION}']"; //  TEMPLATES METHODS локатор тайтл + дескрипшен
 
     public SearchPageObject(AppiumDriver driver)
     {
@@ -23,6 +25,11 @@ public class SearchPageObject extends MainPageObject {
     private static String getResultSearchElement(String substring)
     {
         return SEARCH_RESULT_BY_SUBSTRING_TPL.replace("{SUBSTRING}", substring);
+    }
+
+    private static String getElementByTitleAndDescription(String title, String description)
+    {
+        return SEARCH_BY_TITLE_AND_DESCRIPTION_TPL.replace("{TITLE}", title).replace("{DESCRIPTION}", description);
     }
     /* TEMPLATES METHODS */
 
@@ -35,6 +42,12 @@ public class SearchPageObject extends MainPageObject {
     public void waitForCancelButtonToAppear()   // проверяем наличие кнопки Х ЗАКРЫТИЕ
     {
         this.waitForElementPresent(By.id(SEARCH_CANCEL_BUTTON), "Cannot find search cancel button", 5);
+    }
+
+    public void waitForElementByTitleAndDescription(String title, String description)   // проверяем наличие тайтла и дискрипшена
+    {
+        String search_result_xpath = getElementByTitleAndDescription(title, description);
+        WebElement element = this.waitForElementPresent(By.xpath(search_result_xpath), "Cannot find search result with title" + title + " and " + description, 5);
     }
 
     public void waitForCancelButtonToDisappear()   // проверяем отсутствие кнопки Х ЗАКРЫТИЕ
